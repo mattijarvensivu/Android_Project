7cc808c30ad21cde2a941737955340d8373d52bb
mattijarvensivu <mattijarvensivu93@gmail.com>
2015-11-10 15:10:31 +0200

    lisäystä

--- app/src/main/java/com/example/maza/studentwallet/MainFragment.java
+++ app/src/main/java/com/example/maza/studentwallet/MainFragment.java
@@ -6,6 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 /**
  * Created by maza on 11/3/2015.
@@ -14,13 +19,22 @@ public class MainFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        TextView DateView= (TextView) rootView.findViewById(R.id.DateView);
+        DateView.setText(currentDateTimeString);
+
         return rootView;
+
+
     }
 }
 
